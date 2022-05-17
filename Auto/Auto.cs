8ca77ae48f11e694875772c4c85c4a5798925c85
Владимир Солдатов <cbb110@rambler.ru>
2022-05-17 09:34:42 +0300

    Minor fix

--- Auto/Auto.cs
+++ Auto/Auto.cs
@@ -310,8 +310,8 @@ namespace WindowsFormsApp1
       
                                         
                                     }
-                        
-                          
+
+
                                 }
 
                                 break;
